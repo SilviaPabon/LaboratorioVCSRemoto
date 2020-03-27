2fe9c2caf199ca5b75f9041c93f214dbcf7967ba
SilviaPabon <silvia.pabon.2020@users.noreply.github.com>
2020-03-26 20:01:16 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 19:55:48 2020
+
+@author: --
+"""
+
+#Ejercicio 5
+a = int(input("Ingrese el primer número: "))
+c = int(input("Ingrese el segundo número: "))
+
+print("El resultado es el siguiente: " + str(a*c))
+print("El resultado es el siguiente: " + str(a*2))
