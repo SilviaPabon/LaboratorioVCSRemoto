05e5e17f5e734b6cf90b18e0af1ce483a894d5db
SilviaPabon <silvia.pabon.2020@users.noreply.github.com>
2020-03-26 21:26:38 -0500

    Cambios en if e impresiones, correcciones en paréntesis

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ c = int(input ("Ingrese el valor de c: "))
 d = b**2-4*a*c
 
 if d == 0:
-    print("No existe ecuación cuadrática dentro del dominio de los reales")
+    print("X1 y X2 equivalen a: "+ str(-b/(2*a)))
 elif d > 0:
     print("X1 es igual a: "+ str((-b+(d**1/2))/2*a))
     print("X2 es igual a: "+ str((-b-(d**1/2))/2*a))
 else:
-    print("X1 y X2 equivalen a "+ str(-b/2*a))
+    print("No existe solución dentro del dominio de los reales")
