c4e289580eb5db9391f3c9478734e576641564fb
SilviaPabon <silvia.pabon.2020@users.noreply.github.com>
2020-03-26 21:34:40 -0500

    Solución simple terminada

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ a = int(input ("Ingrese el valor de a: "))
 b = int(input ("Ingrese el valor de b: "))
 c = int(input ("Ingrese el valor de c: "))
 
-d = b**2-4*a*c
+d = (b**2)-(4*a*c)
 
 if d == 0:
     print("X1 y X2 equivalen a: "+ str(-b/(2*a)))
 elif d > 0:
-    print("X1 es igual a: "+ str((-b+(d**1/2))/2*a))
-    print("X2 es igual a: "+ str((-b-(d**1/2))/2*a))
+    print("X1 es igual a: "+ str((-b+(d**(1/2)))/(2*a)))
+    print("X2 es igual a: "+ str((-b-(d**(1/2)))/(2*a)))
 else:
     print("No existe solución dentro del dominio de los reales")
