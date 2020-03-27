6259d2eb1da9c997c7ddb4dacc92bcf35ba124dd
SilviaPabon <silvia.pabon.2020@users.noreply.github.com>
2020-03-26 21:04:24 -0500

    Mi primera modificación

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ Created on Thu Mar 26 19:55:48 2020
 
 @author: --
 """
-
+#%%
 #Ejercicio 5
-a = int(input("Ingrese el primer número: "))
-c = int(input("Ingrese el segundo número: "))
+a = float(input("Ingrese el primer número: "))
+c = float(input("Ingrese el segundo número: "))
 
 print("El resultado es el siguiente: " + str(a*c))
 print("El resultado es el siguiente: " + str(a*2))
+
+#%%
+#Modificación de codificación
+b = int(input("Ingrese el primer número: "))
+print("El resultado del cuadrado del número es: " + str(b**2))
+
+d = float(input("Ingrese el segundo número: "))
+print("La raíz cuadrada del número es: " + str(d**(1/2)))
