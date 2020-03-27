1058abfb2ca13a400845eed5406f360826be15b1
SilviaPabon <silvia.pabon.2020@users.noreply.github.com>
2020-03-26 21:13:01 -0500

    Primera prueba ec cuadrática

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ print("El resultado del cuadrado del número es: " + str(b**2))
 
 d = float(input("Ingrese el segundo número: "))
 print("La raíz cuadrada del número es: " + str(d**(1/2)))
+
+#%%
+#Desarrollo de solución simple
+a = int(input ("Ingrese el valor de a: "))
+b = int(input ("Ingrese el valor de b: "))
+c = int(input ("Ingrese el valor de c: "))
+
+d = b**2-4*a*c
+
+if d < 0:
+    print("No existe ecuación cuadrática dentro del dominio de los reales")
+elif d > 0:
+    print("X1 es igual a: "+ str((-b+(d**1/2))/2*a))
+    print("X2 es igual a: "+ str((-b-(d**1/2))/2*a))
+elif d == 0:
+    print("X1 y X2 equivalen a "+ str(-b/2*a))
