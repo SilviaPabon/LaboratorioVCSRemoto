305570a0bbc713fd53c206c98cf7dbfcc9070e4c
SilviaPabon <silvia.pabon.2020@users.noreply.github.com>
2020-05-08 08:45:41 -0500

    Informe 12 y 13, parte A y B, terminados

--- informe12y13A_SilviaPabon.py
+++ informe12y13A_SilviaPabon.py
@@ -4,84 +4,252 @@ Created on Fri May  1 21:59:47 2020
 
 @author: --
 """
-# ----- Paquetes y librerías ----- #
+# ------------------Importe librerías---------------------#
 
-import random
+import random as rd
 
+#--------------------Funciones---------------------------------#
 
-# -----Funciones----- #
-
-"""Punto 1"""
+#La función imprimir imprime los elementos de una lista y la cantidad de elementos que esta contiene 
 def imprimir(lista):
-    print (lista)
+    print("La lista contiene: " + str(len(lista)) + " elementos")
+    print("Los elementos de la lista son: " + str(lista) + "\n")
+
+#La función generador recibe como parámetros de entrada una lista A y un valor N para retornar una nueva lista con N elementos de la lista A seleccionados aleatoriamente y sin repeticiones.  
+#Para ello, utiliza un ciclo while que agrega los N número de elementos establecidos. Se hace uso de la librería de random, con choice, para escoger los elementos aleatoriamente. Por último se utiliza un condicional para asegurarse que no se repitan elementos.
+def generador(A, N):
+  R =[]
+  num = 0
+  while num < N:
+      Carta = rd.choice(A)
+      if not Carta in R:
+          R.append(Carta)
+          num +=1
+  return R
+
+#La función combinador recibe como parámetros de entrada dos listas A y B, retornando una lista R con los elementos de A y B mezclados. 
+#Para ello, se suman los elementos A y B en una lista n y se hace uso de la librería random, con sample, para organizarlos de forma aleatoria. 
+def combinador(A, B):
+    n = A + B
+    R = rd.sample(n,len(n))
+    return R
+
+# la función lotería, que recibe como parámetros, la lista paquete y la lista premium. 
+#En primer lugar, verifica si paquete tiene objetos repetidos, por medio de un for y una nueva lista, sumando cada vez que se cumpla la condición de que ya está en la lista el elemento. 
+#Luego verifica si en paquete hay cartas premium repetidas. Finalmente se verifica que la cantidad de elementos repetidos en paquete sea mayor o igual a uno y que las cartas premium no estén repetidas más de una vez. Si la anterior condición se cumple se genera la probabilidad de un 10% de que se anexe una carta premium adicional que no esté en paquete. 
+#Si no se cumple, se informa que no se cumplió la condición. 
+def loteria(paquete, premium):
+   
+    repetidas = 0
+    prueba = []
+    for cartas in paquete:
+        if cartas not in prueba:
+            prueba.append(cartas)
+        else:
+            repetidas += 1
+    print(repetidas)
     
-"""Punto 3"""
-def generador(listA, n):
-    R = []
-    for i in listA:
-        R.append(i)
-    R = random.sample(R, n)
-    return R
+    repetidasP = 0
+    for cartas in paquete:
+        if cartas in paquete == "AIVLIS":
+            repetidasP += 1
+        if cartas in paquete == "lEIRBAG":
+            repetidasP += 1
+        if cartas in paquete == "NAILUJ":
+            repetidasP += 1
+        if cartas in paquete == "SOLRAC":
+            repetidasP += 1
+        if cartas in paquete == "ANAID":
+            repetidasP += 1
+    print(repetidasP)
+  
+    if repetidas >= 1 and repetidasP <= 1:
+       probabilidad = rd.randint(0,10)
+       print (probabilidad)
+       if probabilidad == 5:
+           cartaP = rd.choice(premium)
+           print ("hola", cartaP)
+           condicion=0
+           while condicion == 0:
+               cartaP = rd.choice(premium)
+               if cartaP not in paquete:
+                   paquete.append(cartaP)
+                   condicion=1
+              
+    else:
+        print("usted no cumplio las condiciones para entregarle la carta")
+    return(paquete)
 
-"""Punto 5"""
-def combinador(listA, listB):
-    R = []
-    for i in listA:
-        R.append(i)
-    for i in listB:
-        R.append(i)
-    R = random.sample(R, len(R))
-    return R
+#La función cartasPremium identifica cuántas y cuáles fueron las cartas premium obtenidas.
+# Para la cual se hace uso de list comprehension para crear una lista con las cartas premium obtenidas. Después se hace uso de condicionales para imprimir el número de premium que tuvo y cuales fueron. En caso de no haber premium se imprime que no hubo cartas premium. 
+def cartasPremium(jugador):
+    prem = [cartas for cartas in jugador for cartPremium in premium if cartas==cartPremium]
+    num = 0
+    for i in range(len(prem)):
+        num += 1
+    if num != 1 and num != 0:
+        print("Hubo " + str(num) + " cartas premium que fueron " +str(prem) + "\n")
+    elif num==1:
+        print("Hubo " + str(num) + " carta premium que fue " +str(prem) + "\n")
+    else:
+        print("No hubo cartas premium" + "\n")
 
-"""Punto 9"""
-def loteria():
-    for i in range(10):
-        random.choice(range(10))
+#La función cartasRepetidas cuenta cuántas cartas se repitieron. 
+#Para esto hace uso de un ciclo for con condicionales que va almacenando las cartas únicas en una lista y para aquellas que se repiten se van sumando para obtener el total de repetidas. Retorna la cantidad de cartas repetidas. 
+def cartasRepetidas(jugador):
+    repetidas = 0
+    jugador_sin_repetidas = []
+    for cartas in jugador:
+        if cartas not  in jugador_sin_repetidas:
+            jugador_sin_repetidas.append(cartas)
+        else:
+            repetidas += 1
+    return repetidas
 
-# -----Principal----- #
+#La función cantidadCartas recibe como parámetros la lista jugador y mazo (suma de cartas y cartas premium), la función cuenta la cantidad de veces que aparece cada carta de mazo en la lista del jugador, por medio de dos ciclos for y un condicional. 
 
-cartas = ["Payne" , "Hendrix" , "Stone" , "Coffey" , "Whitaker" , "Pope" ,
+def cantidadCartas(jugador, mazo):
+    cantidadC = []
+    cartas
+    suma = 0
+    for element in mazo:
+        almacenador=[]
+        suma = 0
+        for carta in jugador:
+            if carta == element:
+                suma += 1
+        almacenador.append(suma)
+        cantidadC.append([element,almacenador])
+    return cantidadC
+
+#La función cartasAlfabeto toma como parámetro las cartas del jugador y retorna una lista con las letras del abecedario inglés y las cartas que empiezan por dichas letras. 
+#Para esto se hace uso de un ciclo que compara la letra con la que inicia cada carta y la va agregando a lista junto a su letra del alfabeto con la que inicia. 
+def cartasAlfabeto(jugador):
+    cartasAlf = []
+    abecedario = ["A", "B", "C", "D", "E", "F" , "G", "H", "I" , "J", "K", "L", 
+                  "M", "N", "O", "P", "Q", "R", "S", "T" , "U", "V", "W", "X", "Y", "Z"]
+    for letras in abecedario:
+        almacenador=[]
+        for carta in jugador:
+            if carta[0]== letras:
+                almacenador.append(carta)
+        cartasAlf.append([letras,almacenador])
+    return cartasAlf
+
+#La función cartaLong tiene como parámetro de entrada las cartas del jugador y retorna una impresión con la carta de mayor y menor longitud. 
+#Para esto se utiliza un for que recorre elemento por elemento hasta hallar el que tiene mayor y menor longitud. Por último, se imprimen los resultados obtenidos.  
+def cartaLong(jugador):
+    mayor, cartaMayor = len(jugador[0]), jugador[0]
+    menor, cartaMenor = len(jugador[0]), jugador[0]
+    for carta in jugador:
+        if mayor < len(carta) :
+            mayor = len(carta)
+            cartaMayor = carta
+            
+        if menor > len(carta):
+            menor = len(carta)
+            cartaMenor = carta
+            
+    print("La carta de mayor longitud fue " + cartaMayor + " con una longitud de " + str(mayor) + 
+          "; la carta de menor longitud fue " + cartaMenor + " con una longitud de " + str(menor) + "\n")
+
+#La función iniCartPrem tiene como parámetro de entrada las cartas del jugador y retorna una impresión con las cartas que terminan con la(s) carta(s) premium encontradas en las cartas del jugador. 
+#Para lo cual, se utiliza list comprehension para almacenar las cartas premium obtenidas de las cartas del jugador y después se crea un condicional para determinar si hubo premium o no. 
+#En caso de habaer premium se utiliza un ciclo for con condicional para comprar la letra final de cada carta con la inicial de la premium, en caso de que ninguna coincida se imprimirá No hubo ninguna carta que terminará con la letra que inicia la premium. 
+#Si no existe ninguna carta premium en las cartas del jugador entonces se imprimir No hubo premium. 
+def iniCartPrem(jugador):
+    prem = [cartas for cartas in jugador for cartPremium in premium if cartas==cartPremium]
+    if len(prem) != 0:    
+        prem_sin_repetidas = []
+        for carta in prem:
+            if carta not in prem_sin_repetidas:
+                prem_sin_repetidas.append(carta.lower())
+        carta_termina_premium = [carta for prem in prem_sin_repetidas for carta in jugador if carta[-1]==prem[0]]
+        if len(carta_termina_premium) == 0 :
+            print("No hubo ninguna carta que terminará con la letra que inicia la premium" + "\n")
+        else: 
+            print("Las cartas que terminan con la letra que empiezan la(s) premium: " + str(carta_termina_premium)+ "\n")
+            
+    else:
+        print("No hubo premium"+ "\n")
+
+#La función consecutivas recibe como parámetros la lista de jugador y retorna una impresión con las letras repetidas y las cartas a las que corresponden. 
+#Para ello, utiliza un ciclo for que recorre carta por carta, luego otro ciclo for para recorrer letra por letra de la carta y para finalizar un condicional para verificar que la letra actual y la que le sigue son las mismas, es decir, consecutivas. 
+#Por último, se realiza la impresión con los datos recolectados.  
+def consecutivas(jugador):
+    letrasConsecutivas = []
+    cartasConsetuvias = []
+    for carta in jugador:
+        for i in range(len(carta)-1):
+            if carta[i] == carta[i+1]:
+                letrasConsecutivas.append(carta[i])
+                cartasConsetuvias.append(carta)
+    print("Las letras consecutivas son: " + str(letrasConsecutivas) + " en las cartas " + str(cartasConsetuvias))
+    
+#La función numero_letras recibe como parámetro la lista jugador, y crea una lista de listas con las letras del abecedario y la sumatoria total de la cantidad de veces que aparece cada letra del abecedario en los nombres de las cartas de la lista final de la lista jugador, por medio de ciclos y list comprehension. 
+def numero_letras(jugador):
+    player_copy = jugador.copy()
+    abecedario = ["a", "b", "c", "d", "e", "f" , "g", "h", "i" , "j", "k", "l",
+                  "m", "n", "o", "p", "q", "r", "s", "t" , "u", "v", "w", "x", "y", "z"]
+    player_copy_minusculas = [ element.lower() for element in player_copy]
+   
+   
+    numletras = []
+    for i in range(0, len(abecedario)):
+        suma = 0
+        for element in player_copy_minusculas:
+            for caracter in element:
+                if caracter == abecedario[i]:
+                    suma += 1
+       
+        numletras.append(suma)
+   
+    letras_con_numero = [[abecedario[i], numletras[i]] for i in range(0, len(abecedario)) ]
+   
+    return letras_con_numero
+
+cartas = ["Payne" , "Hendrix", "Stone" , "Coffey" , "Whitaker" , "Pope" ,
           "Bleach" , "Arc" , "Fleming" , "Hardin" , "Robiar" , "Mccullough" ,
           "Mooney" , "Reynolds" , "Short" , "Stanton" , "Deadman" ,
-          "Stonehammer" , "Smith" , "Patrick" , "Crane" , "Cargane" , "Powers" , 
-          "Wade" , "Joseph" , "Savage" , "Houston" , "Merritt" , "Nuke" ,
+          "Stonehammer" , "Smith" , "Patrick" , "Crane" , "Cargane" , "Powers"
+          , "Wade" , "Joseph" , "Savage" , "Houston" , "Merritt" , "Nuke" ,
           "Barnett" , "Acosta" , "Duke" , "Sellers" , "Blake" , "Schneider" ,
           "Stone" , "Cannon" , "Garrison" , "Randall" , "Leon" , "Buck" ,
           "Shannon" , "Delaney" , "Mckinney" , "Dodademocles" , "Flowers" ,
           "Whitehead" , "Kirby" , "Park" , "Shannon" , "Vlad" , "Pepin" ,
-          "Mcguire" , "Murray" , "Rush" , "Aramis" , "Fletcher" , "Mcfadden" ,
+          "Mcguire" , "Murray" , "Rush" , "Aramis" , "Fletcher", "Mcfadden" ,
           "Deleon" , "Luke" , "Lindsay" , "Payne" , "Gerbvo" , "Hubbard" ,
-          "Burnett" , "Bryan" , "Ratliff" , "Carlson" , "Parsons" , "Deadmeat" , 
-          "Crimson" , "Wilson" , "Terry" , "Hancock" , "Hightower" , "Burns", 
-          "Austin" , "Nightwalker" , "Thetis" , "Owen" , "Tate" , "Simmons", 
-          "Grant" , "Barber" , "Talos" , "Ashes" , "Alston" , "Clayton" ,
+          "Burnett" , "Bryan" , "Ratliff" , "Carlson" , "Parsons" , "Deadmeat"
+          , "Crimson" , "Wilson" , "Terry" , "Hancock" , "Hightower" , "Burns"
+          , "Austin" , "Nightwalker" , "Thetis" , "Owen" , "Tate" , "Simmons"
+          , "Grant" , "Barber" , "Talos" , "Ashes" , "Alston" , "Clayton" ,
           "Mcbride" , "Huffman" , "Lightbringer" , "Blankenship" , "Higgins" ,
-          "Saint" , "Graham" , "Hodor" , "Ellison" , "Roberts" , "Odom" , "Mann"]
+          "Saint" , "Graham" , "Hodor" , "Ellison" , "Roberts" , "Odom" ,
+          "Mann" ]
 
 premium = ["AIVLIS", "LEIRBAG", "NAILUJ", "SOLRAC", "ANAID"]
 
-
-"""Punto 2"""
 imprimir(cartas)
-print("\n")
 imprimir(premium)
-print("\n")
-
-"""Punto 4"""
 
 jugador = generador(cartas, 10)
-
-"""Punto 6"""
 juego = combinador(cartas, premium)
 
-# 7
 sobre_uno = generador(juego, 5)
 sobre_dos = generador(juego, 5)
 sobre_tres = generador(juego, 5)
 
-# 8
-paquete = combinador(sobre_uno, sobre_dos)+sobre_tres
+
+paquete = combinador(sobre_uno, (sobre_dos + sobre_tres))
 
 
-
-
+jugador = jugador + paquete
+mazo = cartas + premium
+cartasPremium(jugador)
+print("Las cartas repetidas fueron: " + str(cartasRepetidas(jugador)) + "\n")
+print("Carta y número de veces que aparece en cartas del jugador S(respectivamente): ", cantidadCartas(jugador, mazo), "\n")
+print(str(cartasAlfabeto(jugador)) + "\n")
+cartaLong(jugador)
+iniCartPrem(jugador)
+consecutivas(jugador)
+print("Número de veces que aparece cada letra del alfabeto inglés en los nombres de las cartas de lista jugador: ", numero_letras(jugador))
--- informe12y13B_SilviaPabon.py
+++ informe12y13B_SilviaPabon.py
@@ -4,14 +4,16 @@ Created on Sat May  2 00:04:54 2020
 
 @author: --
 """
+#------------------------PARTE B-------------------------------------#
+
+
 # ----- Paquetes y librerías ----- #
 
 import random
 
-
 # -----Funciones----- #
 
-# 13
+# La función combinar utiliza ciclos para combinar los elementos de una lista con las llaves de un diccionario; lo que crea otro diccionario con los elementos combinados como llaves
 def combinar(ponderado, simbolos):
     baraja = {} 
     for element in simbolos:    
@@ -20,28 +22,12 @@ def combinar(ponderado, simbolos):
         
     return baraja
 
-#Está mal
-def revolver(combinatoria):
-    combinar(ponderado, simbolos)
-    
-    simbolos2 = simbolos.copy()
-    simbolos2 = []
-    
-    t = random.sample(simbolos2, len(simbolos2))
-    simbolos2.append(t)
-    
-    baraja = {}     
-    for element in simbolos2:    
-        for key in ponderado:
-            baraja[key+element] = ponderado[key]
-    return baraja
 
-# 14
-def revolver2(combinatoria):
+#La función revolver, tiene por parámetro el diccionario creado antes. 
+#Utiliza la creación de una lista con las llaves del diccionario creado previamente, que luego es combinada aleatoriamente. Se crea un nuevo directorio por medio de ciclos, donde se van juntando la lista aleatorizada junto con los valores del anterior diccionario, siempre y cuando los valores coincidan con el número o la A que tiene la llave en primera posición.  
+def revolver(combinatoria):
     combi = combinatoria.copy()
-    llaves = []
-    for i in combinatoria:
-        llaves.append(i)
+    llaves = [i for i in combinatoria]
     llaves = random.sample(llaves, len(llaves))
     baraja = {}
     for element in llaves:
@@ -53,38 +39,201 @@ def revolver2(combinatoria):
 
     return baraja
 
+#La función repartir reparte cartas de la baraja al jugador y al tallador. Utiliza un condicional para saber si el jugador y el tallador no poseen cartas
+#Si no tienen ninguna carta se reparten 2, utilizando un ciclo for para ello, y se eliminan esas cartas repartidas.
+#Si ya tienen cartas solo les reparte 1 y también se elimina de la baraja
 
-def generador(listA, n):
-    R = []
-    for i in listA:
-        R.append(i)
-    R = random.sample(R, n)
-    return R
+def repartir(baraja_usuario ,carta):
+    baraja_copia = carta.copy()
+    cartasBaraja = [key for key in baraja_copia]
+    cartasRepartidas = []
+    
+    if len(baraja_usuario) == 0 :
+        for i in range(2):
+            cart = random.choice(cartasBaraja)
+            cartasRepartidas.append(cart)
+            del baraja_copia[cart]
+    else:
+        cartasRepartidas = baraja_usuario
+        cart = random.choice(cartasBaraja)
+        cartasRepartidas.append(cart)
+        del baraja_copia[cart]
+        
+    return cartasRepartidas
 
+#La función sumar_cartas realiza la suma de los valores de las cartas. Hace uso de ciclos for para ir sumando cada valor.
+#A su vez, cálcula si en la primera barraja hay un BlackJack y cuando A vale 1 u 11.  
+
+def sumar_cartas(cartas):
+    blackjack = False
+    AS = False
+    
+    suma = 0
+
+    for carta in cartas:
+        for cart in baraja:
+            if carta==cart :
+                suma += baraja[cart]
+    
+    for elemento in cartas:
+        for valor in elemento:
+            if valor == "A":
+                AS = True
+            if valor == "J" or valor == "K" or valor == "Q" :
+                blackjack = True
+                
+    if (blackjack and AS) and len(cartas)==2 :
+        suma = 21
+    
+    if AS == True and blackjack == False:
+        suma21 = suma-1
+        if (suma21+11) <= 21 :
+            suma=suma21+11
+            
+    return suma
+
+#La función mostrar muestra las cartas del jugador o tallador, junto su suma y si sobrepasaron el 21, significando una derrota.
+def mostrar(lista):
+    if sumar_cartas(lista) > 21 :
+        return "Has perdido"
+    else:
+        return ("Las cartas que posee son: " + str(lista) + " que suman " + str(sumar_cartas(lista)))
+    
+#La función tomar_cartas_jugador toma cartas de la baraja para darselo a las cartas del usuario hasta que el no decida tomar más o se pase de 21
+#Se utiliza un ciclo while para ir preguntale al jugador si quiere tomar una carta de la baraja(con la función repartir) hasta que diga no o se pase de 21
+def tomar_cartas_jugador(jugador):
+    
+    print("\n" + "Jugador: " + str(mostrar(jugador)))
+    if sumar_cartas(jugador) == 21 :
+        return jugador
+    tomar = input("¿Quiere tomar otra carta? ")
+    cartas_jugador = jugador
+    while (tomar == "si" or tomar == "yes") :     
+        
+        cartas_jugador = repartir(cartas_jugador, baraja)
+        print(mostrar(cartas_jugador))  
+        
+        if mostrar(cartas_jugador) == "Has perdido":
+            break
+        tomar = input("¿Quiere tomar otra carta? ")
+
+    return cartas_jugador
+#La función tomar_cartas_tallador toma las cartas para el tallador o repartidor hasta que supere, iguale al jugador o sobrepase los 21.
+#utiliza un ciclo while para tomar cartas de la baraja hasta que se cumplan una de las condiciones ya mencionadas.
+def tomar_cartas_tallador(tallador):
+    
+    print("\n" + "Tallador: " + str(mostrar(tallador)))
+    if sumar_cartas(tallador) == 21 :
+        return tallador
+    if sumar_cartas(cartas_jugador) > 21:
+        return tallador
+    cartas_tallador = tallador
+    while (sumar_cartas(cartas_tallador) < sumar_cartas(cartas_jugador) 
+           or sumar_cartas(cartas_tallador) == sumar_cartas(cartas_jugador)) :
+        
+        cartas_tallador = repartir(cartas_tallador, baraja)
+        
+        if sumar_cartas(cartas_tallador) > 21:
+            break
+        print("\n" + "Tallador: " + str(mostrar(tallador)))
+        
+    return cartas_tallador
+
+#La función ganador decide quién fue el ganador de la partida. Haciendo uso de las anteriores funciones para decidir el ganador.
+def ganador(jugador, tallador):
+    if mostrar(cartas_jugador) == "Has perdido" :
+        return "El tallador gana"
+    elif sumar_cartas(jugador) > sumar_cartas(tallador):
+        return "El jugador gana"
+    elif sumar_cartas(jugador) == 21 :
+        return "El jugador gana"
+    elif sumar_cartas(tallador) == 21 :
+        return "El tallador gana"
+    elif sumar_cartas(tallador) > 21 :
+        return "El jugador gana"
+    elif sumar_cartas(jugador) < sumar_cartas(tallador) :
+        return "El tallador gana"
+
+#La función new_partida crea una nueva partida si el jugador elige continuar jugando. Utiliza un ciclo while para jugar tantas partidas como el jugador quiera.
+def new_partida(part):
+    cartas_jugador = []
+    cartas_tallador = []
+    ganadas = ganada
+    perdidas = perdida
+    partidas_jugadas = 1
+    while part == "YES" :
+        ponderado = {"A": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "J": 10, "Q": 10, "K": 10}
+        simbolos = ["C", "D", "T", "P"]
+        baraja = combinar(ponderado, simbolos)
+        baraja = revolver(baraja)
+        cartas_jugador = repartir(cartas_jugador, baraja)
+        cartas_tallador = repartir(cartas_tallador, baraja)
+        
+        cartas_jugador = tomar_cartas_jugador(cartas_jugador)
+        cartas_tallador = tomar_cartas_tallador(cartas_tallador)
+        
+        winner = ganador(cartas_jugador,cartas_tallador)
+        print(ganador(cartas_jugador,cartas_tallador))
+        partidas_jugadas += 1
+        if winner == "El jugador gana":
+            ganadas += 1
+        else:
+            perdidas += 1
+            
+        part = input("¿Quiere jugar otra partida? ")
+        
+    print("Jugaste " + str(partidas_jugadas) + " de las cuales ganaste " + str(ganadas) + " y perdiste " + str(perdidas))
+        
+        
 # -----Principal----- #
 
 # 11
-ponderado = {"A": [1],
-             "2": [2],
-             "3": [3],
-             "4": [4],
-             "5": [5],
-             "6": [6],
-             "7": [7],
-             "8": [8],
-             "9": [9],
-             "J": [10],
-             "Q": [10],
-             "K": [10]}
+ponderado = {"A": 1,
+             "2": 2,
+             "3": 3,
+             "4": 4,
+             "5": 5,
+             "6": 6,
+             "7": 7,
+             "8": 8,
+             "9": 9,
+             "J": 10,
+             "Q": 10,
+             "K": 10}
 
 # 12
 simbolos = ["C", "D", "T", "P"]
 
 # 13
-combinatoria = combinar(ponderado, simbolos)
-print(combinatoria)
+baraja = combinar(ponderado, simbolos)
+#14
+baraja = revolver(baraja)
 
-# 14 - 15
-cartas_jugador = revolver2(combinatoria)
-cartas_tallador = revolver2(combinatoria)
-print (cartas_jugador)
+
+# 15
+cartas_jugador = []
+cartas_tallador = []
+
+cartas_jugador = repartir(cartas_jugador, baraja)
+cartas_tallador = repartir(cartas_tallador, baraja)
+
+
+cartas_jugador = tomar_cartas_jugador(cartas_jugador)
+cartas_tallador = tomar_cartas_tallador(cartas_tallador)
+print(cartas_tallador)
+
+winner = print(ganador(cartas_jugador,cartas_tallador))
+
+partidas_jugadas = 1
+ganada = 0
+perdida = 0
+if winner == "El jugador gana":
+    ganada += 1
+else:
+   perdida += 1
+
+game = input("¿Quiere jugar otra partida? ")
+if game=="YES":
+    new_partida(game)
+else:
+  print("Jugaste " + str(partidas_jugadas) + " de las cuales ganaste " + str(ganada) + " y perdiste " + str(perdida))
